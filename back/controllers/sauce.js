cf67dbcdbddbd7a4b7705a1c9ca7077925b29c4a
Theophile Bancal <tbancal@free.fr>
2022-12-16 20:11:19 +0100

    add getOneSauce routes and fonction

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -31,6 +31,13 @@ exports.getAllSauce = (req, res, next) => {
 };
 
 
+exports.getOneSauce = (req, res, next) => {
+Sauce.findOne({ _id: req.params.id })
+  .then(sauce => res.status(200).json(sauce))
+  .catch(error => res.status(404).json({ error }));
+};
+
+
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ? {
     ...JSON.parse(req.body.sauce),
--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -1,11 +1,13 @@
-const multer = require("../middleware/multer-config");
 const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
 const sauceCtrl = require("../controllers/sauce")
 
 router.get('/', auth, sauceCtrl.getAllSauce); 
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
